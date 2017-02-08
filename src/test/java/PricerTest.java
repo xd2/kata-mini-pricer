978831accf61e9359860f2774a294a4613796bb5
x4v13r <xavier.delestr@gmail.com>
2017-02-08 07:16:44 +0100

    Step1.3 : test refacto-> givenWhenThen style, add negative volatility test.

--- src/test/java/PricerTest.java
+++ src/test/java/PricerTest.java
@@ -5,13 +5,27 @@ import java.time.LocalDate;
 
 public class PricerTest {
 
-    double INITIAL_PRICE = 50;
-    double VOLATILITY = 2;
+    @Test
+    public void shouldPrice51_WhenMaturityEquals1_AndStrikeEquals50_WithVolatilityEquals2(){
+        //Given
+        double initialPrice = 50;
+        double volatility = 2;
+        //When
+        Pricer pricer = new Pricer();
+        double price = pricer.price(initialPrice,volatility, LocalDate.now().plusDays(1));
+        //Then
+        Assert.assertEquals(51,price,0);
+    }
 
     @Test
-    public void shouldPrice51WhenMaturityEquals1AndStrikeEquals50(){
+    public void shouldPrice49_WhenMaturityEquals1_AndStrikeEquals50_WithVolatilityEqualsMinus2(){
+        //Given
+        double initialPrice = 50;
+        double volatility = -2;
+        //When
         Pricer pricer = new Pricer();
-        double price = pricer.price(INITIAL_PRICE,VOLATILITY, LocalDate.now().plusDays(1));
-        Assert.assertEquals(51,price,0);
+        double price = pricer.price(initialPrice,volatility, LocalDate.now().plusDays(1));
+        //Then
+        Assert.assertEquals(49,price,0);
     }
 }
